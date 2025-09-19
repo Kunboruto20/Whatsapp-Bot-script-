a8db1be50013ee5da8ab2efdb7255f4e87c63704
Kunboruto20 <gyovanyy147@gmail.com>
2025-09-19 07:29:01 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const { connectToWhatsApp } = require('./auth');
-const setupCommands = require('./commands');
-const config = require('./config');
-
-connectToWhatsApp().then((sock) => {
-  setupCommands(sock);
-});
